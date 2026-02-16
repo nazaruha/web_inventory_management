cd22e5609fcad57006683b11597dca3d5aa24bd1
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2026-02-16 16:12:03 +0100

    feat: add Prettier and ESLint configuration with unused imports plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,45 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import nextVitals from 'eslint-config-next/core-web-vitals'
+import nextTs from 'eslint-config-next/typescript'
+import unusedImports from 'eslint-plugin-unused-imports'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
 
-export default eslintConfig;
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    '**/node_modules/**',
+  ]),
+
+  {
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      // Turn off standard unused vars to let the plugin handle it
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+
+      // AUTO-REMOVE imports on save
+      'unused-imports/no-unused-imports': 'error',
+
+      // Warn about unused variables (but don't delete them automatically)
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+])
+
+export default eslintConfig
