eb92a8c54667eb4c95eddfba74b87fa9857dd0f1
Nazarii Fedun <fedunnazarii20122001@gmail.com>
2026-02-16 14:14:43 +0100

    feat: implement Prisma client and PostgreSQL pool setup for database connection

---
+++ lib/prisma.ts
@@ -0,0 +1,17 @@
+import { PrismaClient } from "../app/generated/prisma/client"
+import { PrismaPg } from "@prisma/adapter-pg"
+import { Pool } from "pg"
+
+// Neon requires SSL. This setup ensures the pool is ready before Prisma starts.
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true, // Explicitly enforce SSL for Neon
+})
+
+const adapter = new PrismaPg(pool)
+
+const prisma = new PrismaClient({
+  adapter,
+})
+
+export { prisma, pool }
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,5 @@
-import { PrismaClient } from "../app/generated/prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
+import { pool, prisma } from '@/lib/prisma';
 import 'dotenv/config';
-
-// Neon requires SSL. This setup ensures the pool is ready before Prisma starts.
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true, // Explicitly enforce SSL for Neon
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-});
 
 export async function main() {
   const userId = '6b8a38a5-6b8b-4856-9fd0-e18df0782b01';
